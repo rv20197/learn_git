e1efca81cefcc290a449aa0cc1b6596088745039
Vatsal Rajgor <rv20197@gmail.com>
2021-02-05 15:36:29 +0530

    init commit

---
+++ tut3.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+// Coments and variables 
+/*
+This 
+is
+a 
+multiline 
+comment
+*/
+int main(){
+    int a;
+    cout<<"Enter a number ";
+    cin>>a;
+    cout<<"Hello World "<<a;
+    return 0;
+}
